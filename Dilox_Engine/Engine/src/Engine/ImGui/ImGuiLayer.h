6701e28a9e146ffa6c0405e87bc48d48ed7d1bb4
Lobinux <lobinux@digilearnnials.com>
2022-08-31 12:12:50 -0300

    Implement ImGUI events

--- Dilox_Engine/Engine/src/Engine/ImGui/ImGuiLayer.h
+++ Dilox_Engine/Engine/src/Engine/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Engine/Layer.h"
+#include "Engine/Events/ApplicationEvent.h"
+#include "Engine/Events/MouseEvent.h"
+#include "Engine/Events/KeyEvent.h"
 
 namespace DiloxGE
 {
@@ -8,6 +11,16 @@ namespace DiloxGE
 	{
 	private:
 		float m_Time = 0.0f;
+
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
 
 	public:
 		ImGuiLayer();
