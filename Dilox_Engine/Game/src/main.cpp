5d166883197c1fc1223df19f7bd576fddfe9a44e
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-09-06 23:10:43 -0300

    Updated Sandbox

--- Dilox_Engine/Game/src/main.cpp
+++ Dilox_Engine/Game/src/main.cpp
@@ -10,12 +10,21 @@ public:
 
 	void OnUpdate() override
 	{
-		DGE_INFO("ExampleLayer::Update");
+		//DGE_INFO("ExampleLayer::Update");
+
+		if (DiloxGE::Input::IsKeyPressed(DGE_KEY_TAB))
+		{
+			DGE_TRACE("Tab key is pressed!");
+		}
 	}
 
 	void OnEvent(DiloxGE::Event& event) override
 	{
-		DGE_TRACE("{0}", event);
+		if (event.GetEventType() == DiloxGE::EventType::KeyPressed)
+		{
+			DiloxGE::KeyPressedEvent& e = (DiloxGE::KeyPressedEvent&)event;
+			DGE_TRACE("{0}", (char)e.GetKeyCode()); //Si tocas la T, se muestra una T porque esta en ASCII
+		}
 	}
 };
 
@@ -32,5 +41,5 @@ class Sandbox : public DiloxGE::BaseGame
 
 DiloxGE::BaseGame* DiloxGE::CreateBaseGame()
 {
-	return new Sandbox();
+	return new Sandbox(); 
 }
