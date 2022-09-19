0edd3ef171ccfa0c26caf75881a00cfd1f97329d
PabloGoldman <pablo.goldmanarca@gmail.com>
2022-09-18 22:16:03 -0300

    Added Entity2D class

---
+++ Dilox_Engine/Engine/src/Tools/Entity2D.cpp
@@ -0,0 +1,16 @@
+#include "dgepch.h"
+#include "Entity2D.h"
+
+namespace DiloxGE
+{
+	Entity2D::Entity2D(Renderer2D* renderer) : Entity(renderer)
+	{
+
+	}
+
+	Entity2D::~Entity2D()
+	{
+
+	}
+}
+
---
+++ Dilox_Engine/Engine/src/Tools/Entity2D.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Tools/Entity.h"
+
+namespace DiloxGE
+{
+	class Entity2D : public Entity
+	{
+	private:
+
+	public:
+		Entity2D(Renderer2D* renderer);
+		~Entity2D();
+
+	};
+}
+
