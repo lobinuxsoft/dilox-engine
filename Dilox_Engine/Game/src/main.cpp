d9ab2267fe43d01cd9103b4541ca9c9f45dacfc5
Lobinux <lobinux@digilearnnials.com>
2022-09-25 05:38:18 -0300

    Add Transforms

--- Dilox_Engine/Game/src/main.cpp
+++ Dilox_Engine/Game/src/main.cpp
@@ -2,6 +2,8 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public DiloxGE::Layer
 {
 public:
@@ -44,10 +46,10 @@ public:
 		m_SquareVA.reset(DiloxGE::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<DiloxGE::VertexBuffer> squareVB;
@@ -70,7 +72,8 @@ public:
 			layout(location = 1) in vec4 a_Color;
 			
 			uniform mat4 u_ViewProjection;
-			
+			uniform mat4 u_Transform;
+
 			out vec3 v_Position;
 			out vec4 v_Color;
 
@@ -78,7 +81,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -105,13 +108,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 			
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -156,7 +160,18 @@ public:
 
 		DiloxGE::Renderer::BeginScene(m_Camera);
 
-		DiloxGE::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (size_t y = 0; y < 20; y++)
+		{
+			for (size_t x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f,y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				DiloxGE::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+
 		DiloxGE::Renderer::Submit(m_Shader, m_VertexArray);
 
 		DiloxGE::Renderer::EndScene();
