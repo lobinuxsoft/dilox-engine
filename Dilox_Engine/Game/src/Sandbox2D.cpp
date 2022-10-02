0a628e2b19cc24f1dd871d0788ca87969337213c
Lobinux <lobinux@digilearnnials.com>
2022-10-02 17:38:21 -0300

    2D Renderer Transform

--- Dilox_Engine/Game/src/Sandbox2D.cpp
+++ Dilox_Engine/Game/src/Sandbox2D.cpp
@@ -4,8 +4,6 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-#include "Platform/OpenGL/OpenGLShader.h"
-
 Sandbox2D::Sandbox2D() : Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f, true) { }
 
 void Sandbox2D::OnAttach() 
@@ -28,18 +26,21 @@ void Sandbox2D::OnUpdate(DiloxGE::Timestep ts)
 	DiloxGE::RenderCommand::Clear();
 
 	DiloxGE::Renderer2D::BeginScene(m_CameraController.GetCamera());
-	DiloxGE::Renderer2D::DrawQuad({ 0.0f,0.0f }, { 1.0f,1.0f }, m_SquareColor);
+	DiloxGE::Renderer2D::DrawQuad({ -1.0f,0.0f }, { 0.8f,0.8f }, m_SquareColor);
+	DiloxGE::Renderer2D::DrawQuad({ 0.5f,-0.5f }, { 0.5f,0.75f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 	DiloxGE::Renderer2D::EndScene();
-
-	// TODO: Add this functions >>=> Shader::SetMat4, Shader::SetFloat4
-	// std::dynamic_pointer_cast<DiloxGE::OpenGLShader>(m_FlatColorShader)->Bind();
-	// std::dynamic_pointer_cast<DiloxGE::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
 }
 
 void Sandbox2D::OnImGuiRender()
 {
 	ImGui::Begin("Settings");
-	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor), ImGuiColorEditFlags_PickerHueWheel | ImGuiColorEditFlags_AlphaBar);
+
+	ImGui::ColorEdit4(
+		"Square Color",
+		glm::value_ptr(m_SquareColor),
+		ImGuiColorEditFlags_PickerHueWheel | ImGuiColorEditFlags_AlphaBar
+	);
+
 	ImGui::End();
 }
 
